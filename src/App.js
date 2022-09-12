553bf8d50b2cbfeffa24d06295d5c43f103f8d17
dhara-msys <111041865+dhara-msys@users.noreply.github.com>
2022-09-12 20:11:25 +0530

    Add files via upload
    added Switching between locales

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import React,{useState} from "react";
 import './App.css';
 import { ExampleComponent } from './components/ExampleComponent.js';
+import i18n from "i18next";
 
 function App() {
+  const [language, setLanguage] = useState('en');
+  const handleOnclick=(e)=>{
+    e.preventDefault();
+    setLanguage(e.target.value);
+    i18n.changeLanguage(e.target.value);
+  }
   return (
     <div className="App">
+       <button value='arab' onClick={handleOnclick}>
+        Arabic
+     </button>
+     <button value='en' onClick={handleOnclick}>
+        English
+     </button>
+    <button value='gu' onClick={handleOnclick}>
+    Gujarati
+    </button>
+    <button value='zh' onClick={handleOnclick}>
+    Chinese
+    </button>
      <ExampleComponent />
-     <p>*used tutorial bellow link  </p>
-     <a target="_blank" href='https://lokalise.com/blog/how-to-internationalize-react-application-using-i18next/'>https://lokalise.com/blog/how-to-internationalize-react-application-using-i18next/</a>
+     
+      {/*<p>used tutorial bellow link  </p>
+      <a target="_blank" href='https://lokalise.com/blog/how-to-internationalize-react-application-using-i18next/'>https://lokalise.com/blog/how-to-internationalize-react-application-using-i18next/</a>*/}
     </div>
   );
 }
